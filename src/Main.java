03c61d82c607153d947bafbf751c5c94ca8ebf8b
Heming233 <heming7159@163.com>
2021-07-10 09:05:45 +0800

    E:/Java/test233

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String str1="290 ";
+        String str2=str1.trim();
+        System.out.println(Integer.valueOf(str2));
+    }
+}
